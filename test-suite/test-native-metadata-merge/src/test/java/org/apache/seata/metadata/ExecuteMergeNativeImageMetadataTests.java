169ad5cf0fd4cdbcdd02eb60e8ee4c9ef9cac2c3
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2026-08-09 22:21:06 +0800

    style: fix line wrapping and remove trailing blank line in test

--- test-suite/test-native-metadata-merge/src/test/java/org/apache/seata/metadata/ExecuteMergeNativeImageMetadataTests.java
+++ test-suite/test-native-metadata-merge/src/test/java/org/apache/seata/metadata/ExecuteMergeNativeImageMetadataTests.java
@@ -102,8 +102,8 @@ class ExecuteMergeNativeImageMetadataTests {
         // Generated metadata from the build output directory
         String source = PROJECT_BASE + "/target/native-image-config/reachability-metadata.json";
         // Canonical metadata file in the server source tree
-        String target =
-                PROJECT_BASE + "/server/src/main/resources/META-INF/native-image/org.apache.seata/seata-server/reachability-metadata.json";
+        String target = PROJECT_BASE
+                + "/server/src/main/resources/META-INF/native-image/org.apache.seata/seata-server/reachability-metadata.json";
 
         JsonNode sourceNode = objectMapper.readTree(new File(source));
         JsonNode targetNode = objectMapper.readTree(new File(target));
@@ -114,5 +114,4 @@ class ExecuteMergeNativeImageMetadataTests {
         // Write back the merged result with pretty printing
         objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File(target), targetNode);
     }
-
 }
